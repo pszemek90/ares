fb462a0e61725594d6d78101baac276b038a1476
pszemek90 <me@example.com>
2020-10-18 09:50:07 +0200

    Initial commit

---
+++ src/main/java/com/pszemek/module/books/BooksController.java
@@ -0,0 +1,40 @@
+package com.pszemek.module.books;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class BooksController {
+
+	@Autowired
+	private BooksRepository booksRepository;
+
+	@GetMapping(value = "/api/books/{id}")
+	public BooksEntity getBook(@PathVariable Long id) {
+		return booksRepository.findById(id).get();
+	}
+
+	@GetMapping("/api/books")
+	public List<BooksEntity> getBooks() {
+		return booksRepository.findAll();
+	}
+
+	@PostMapping("/api/books")
+	public BooksEntity newBooks(@Valid @RequestBody BooksEntity booksEntity) {
+		return booksRepository.saveAndFlush(booksEntity);
+	}
+
+	@PutMapping("/api/books/{id}")
+	public BooksEntity updateBooks(@PathVariable Long id,
+	                               @RequestBody BooksEntity booksEntity) {
+		return booksRepository.saveAndFlush(booksEntity.setId(id));
+	}
+
+	@DeleteMapping("/api/books/{id}")
+	public void deleteBooks(@PathVariable Long id) {
+		booksRepository.deleteById(id);
+	}
+}
---
+++ src/main/java/com/pszemek/module/home/HomeService.java
@@ -0,0 +1,10 @@
+package com.pszemek.module.home;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+	public GreetingDto getGreetings() {
+		return new GreetingDto("Hello world");
+	}
+}
